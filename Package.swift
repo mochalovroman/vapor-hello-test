50d13a96d5b4ff1d39dea6e336c2cc2efc91c114
Roman Mochalov <mochalov@improveitgroup.com>
2017-05-14 01:14:27 +0700

    postgree was added

--- Package.swift
+++ Package.swift
@@ -5,6 +5,15 @@ import PackageDescription
 let package = Package(
     name: "Hello",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
-    ]
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+        ]
 )
