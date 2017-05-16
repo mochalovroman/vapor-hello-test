632d3352d60799b874420f9a154d43bea056a840
Roman Mochalov <mochalov@improveitgroup.com>
2017-05-16 16:07:46 +0700

    exclude was deleted

--- Package.swift
+++ Package.swift
@@ -7,13 +7,5 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
-    ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-        "Tests",
-        ]
+    ]
 )
